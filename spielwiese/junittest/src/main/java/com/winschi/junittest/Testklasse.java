adc2dcb751773ae7b32cf706ceada1ca3745c23c
Wilfried M <winschi1@web.de>
2018-11-10 22:22:37 +0100

    Inital commit

---
+++ spielwiese/junittest/src/main/java/com/winschi/junittest/Testklasse.java
@@ -0,0 +1,11 @@
+package com.winschi.junittest;
+
+public class Testklasse {
+	public String getHelloWorld() {
+		return "Hello World";
+	}
+	public void printLn() {
+		System.out.println(getHelloWorld());
+	}
+
+}
---
+++ spielwiese/junittest/src/test/java/com/winschi/junittest/TestklasseTest.java
@@ -0,0 +1,14 @@
+package com.winschi.junittest;
+
+import org.junit.Test;
+
+public class TestklasseTest {
+
+	@Test
+	public void testPrintLn() {
+		Testklasse testklasse = new Testklasse();
+		String value = testklasse.getHelloWorld();
+
+	}
+
+}
